856e03d433b15d6db9270d0a68e6f0ff0d530a5d
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2023-06-26 17:41:37 +0300

    todos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Header from "./components/Header/Header";
 import {useTelegram} from "./hooks/useTelegram";
 
 
 function App() {
   const {onToggleButton, tg} = useTelegram()
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     tg.ready()
+  },[])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setData(json))
   },[])
 
 
@@ -16,6 +23,9 @@ function App() {
     <div className="App">
     <Header />
       <button onClick={onToggleButton}>toggle</button>
+      <ul>
+        {data.slice(0,10).map(item => <li>{item.title}</li>)}
+      </ul>
     </div>
   );
 }
